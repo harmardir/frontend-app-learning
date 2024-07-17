4a74e8906b91dc70440c6a3f273aea0bf2e70f2f
Harout Mardirossian <harmardir@gmail.com>
2024-07-18 00:03:02 +0300

    Editing hooks.js in sequence-navigation

--- src/courseware/course/sequence/sequence-navigation/hooks.js
+++ src/courseware/course/sequence/sequence-navigation/hooks.js
@@ -8,23 +8,24 @@ export function useSequenceNavigationMetadata(currentSequenceId, currentUnitId) 
   const courseStatus = useSelector(state => state.courseware.courseStatus);
   const sequenceStatus = useSelector(state => state.courseware.sequenceStatus);
 
-  // Early return if data isn't fully loaded, IDs are missing, or sequence/unit data is incomplete
   if (courseStatus !== 'loaded' || sequenceStatus !== 'loaded' || !currentSequenceId || !currentUnitId || !sequence || !sequence.unitIds) {
     return { isFirstUnit: false, isLastUnit: false, totalUnits: 0, completedUnits: 0 };
   }
 
-  // Fetch all units at once to adhere to rules of hooks
-  const units = sequence.unitIds.map(unitId => useModel('units', unitId));
-
-  const totalUnits = sequence.unitIds.length;
+  const units = sequence.unitIds.map(unitId => useModel('units', unitId)); // Consider refactoring this part
+  const totalUnits = sequence?.unitIds?.length || 0;
   const completedUnits = units.reduce((acc, unit) => acc + (unit?.complete ? 1 : 0), 0);
 
   const isFirstSequence = sequenceIds.indexOf(currentSequenceId) === 0;
-  const isFirstUnitInSequence = sequence.unitIds.indexOf(currentUnitId) === 0;
-  const isFirstUnit = isFirstSequence && isFirstUnitInSequence;
+  const isFirstUnitInSequence = sequence?.unitIds?.indexOf(currentUnitId) === 0;
   const isLastSequence = sequenceIds.indexOf(currentSequenceId) === sequenceIds.length - 1;
-  const isLastUnitInSequence = sequence.unitIds.indexOf(currentUnitId) === sequence.unitIds.length - 1;
-  const isLastUnit = isLastSequence && isLastUnitInSequence;
+  const isLastUnitInSequence = sequence?.unitIds?.indexOf(currentUnitId) === sequence.unitIds.length - 1;
 
-  return { isFirstUnit, isLastUnit, totalUnits, completedUnits };
+  return {
+    isFirstUnit: isFirstSequence && isFirstUnitInSequence,
+    isLastUnit: isLastSequence && isLastUnitInSequence,
+    totalUnits,
+    completedUnits
+  };
 }
+
