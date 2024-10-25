5c56d5cf30e99e2cbbc615fb69c90003071482a6
Harout Mardirossian <harmardir@gmail.com>
2024-10-25 12:11:16 +0300

    editing sequence navigation

--- src/courseware/course/sequence/sequence-navigation/SequenceNavigation.jsx
+++ src/courseware/course/sequence/sequence-navigation/SequenceNavigation.jsx
@@ -83,7 +83,7 @@ function SequenceNavigation({
   const prevArrow = isRtl(getLocale()) ? ChevronRight : ChevronLeft;
 
   return sequenceStatus === LOADED && (
-    <nav id="courseware-sequenceNavigation" className={classNames('sequence-navigation', className)} style={{ width: shouldDisplayNotificationTriggerInSequence ? '90%' : null }}>
+    <nav id="courseware-sequenceNavigation" className={classNames('sequence-navigation', className)} style={{ width:'auto' }}>
       <Button variant="link" className="previous-btn" onClick={previousSequenceHandler} disabled={isFirstUnit} iconBefore={prevArrow}>
         {shouldDisplayNotificationTriggerInSequence ? null : intl.formatMessage(messages.previousButton)}
       </Button>
